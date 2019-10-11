515949aed23cfc031242bb8dba335f25c56a5354
Huet Bryan <21701042@C304L-007D09.campus.unicaen.fr>
2019-10-11 11:33:41 +0200

    Add interface princip and verifier class

---
+++ filrouge/Verifier.java
@@ -0,0 +1,27 @@
+package filrouge;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Verifier{
+  protected ArrayList<Constraint> list;
+
+  public Verifier(){
+    this.list = new ArrayList<>();
+  }
+
+  public void add(Constraint contrainte){
+    if ( ! this.list.contains(contrainte)){
+      this.list.add(contrainte);
+    }
+  }
+
+  public boolean verify(HashMap<Activity, Integer> hmap){
+    for(Constraint c: this.list){
+      if ( ! c.isSatisfiedBySchedule(hmap)){
+        return false;
+      }
+    }
+    return true;
+  }
+
+}
